410cceb9b451dbf1f12f8ddc3e20762204f7deb9
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-09-17 17:28:34 +0200

    Justice

--- WorldClock_2.0.c
+++ WorldClock_2.0.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
 int main()
 {
+	//
     FILE *cmd=popen("TZ=Pacific/Fakaofo date", "r");
-    char result[24]={0x0};
+
+	//
+    char result[30]={0x0};
+
+	//
     while (fgets(result, sizeof(result), cmd) !=NULL)
            printf("%s\n", result);
+	
+	//
     pclose(cmd);
+
+	//
     return 0;
 }
