be7fdeb883a0d128747b7241c5ed5e7528c5ab56
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-09-20 18:34:00 +0200

    SFuture

--- WorldClock_2.0/WorldClock_2.0.c
+++ WorldClock_2.0/WorldClock_2.0.c
@@ -98,31 +98,3 @@ char* getDateTimeForWishedTZ(char* wishedTZ)
 	//
     return dtChoosenTZ;
 }
-
-//
-/*int main()
-{
-	//
-	enum OS currentOS = getCurrentOS();
-
-	//
-	char* arrayOfTZ = getListOfAllKnownTZ();
-
-	//
-	for (int i = 0; i < 3; i++)
-	{
-		printf("%c\n", arrayOfTZ[i]);
-	}
-
-	//
-	printf("\n\n\n\n");
-
-	//
-	char* resultDTForWishedTZ = getDateTimeForWishedTZ("Pacific/Honolulu");
-
-	//
-	printf("%s", resultDTForWishedTZ);
-
-	//
-	return 0;
-}*/
--- main.c
+++ main.c
@@ -6,3 +6,31 @@ int main()
     //
     return 0;
 }
+
+//
+/*int main()
+{
+	//
+	enum OS currentOS = getCurrentOS();
+
+	//
+	char* arrayOfTZ = getListOfAllKnownTZ();
+
+	//
+	for (int i = 0; i < 3; i++)
+	{
+		printf("%c\n", arrayOfTZ[i]);
+	}
+
+	//
+	printf("\n\n\n\n");
+
+	//
+	char* resultDTForWishedTZ = getDateTimeForWishedTZ("Pacific/Honolulu");
+
+	//
+	printf("%s", resultDTForWishedTZ);
+
+	//
+	return 0;
+}*/
