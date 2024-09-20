1073fd332f3a7b6cfbd880e85f60f6745b8cb74d
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-09-20 19:05:38 +0200

    BzB

--- WorldClock_2.0/WorldClock_2.0.h
+++ WorldClock_2.0/WorldClock_2.0.h
@@ -1,9 +1,4 @@
 //
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-
-//
 enum OS {undefined, unix, macOS, windows, linux, android, freeBSD};
 
 //
--- tests/main.c
+++ tests/main.c
@@ -1,4 +1,7 @@
 //
+#include <stdio.h>
+
+//
 #include "../WorldClock_2.0/WorldClock_2.0.h"
 
 //
