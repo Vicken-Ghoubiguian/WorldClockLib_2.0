fa1cda73f8296370219d1d2056e22b686e278f8e
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-10-14 18:22:23 +0200

    uIOIOJKK

---
+++ WorldClock_2.0/WorldClock_2.0.c
@@ -0,0 +1,142 @@
+//
+#include "WorldClock_2.0.h"
+
+// Function 'displaytListOfAllKnownTZ' to display all available timezones
+void displaytListOfAllKnownTZ()
+{
+	// 
+	char* command = "awk '/^Z/ { print $2 }; /^L/ { print $3 }' /usr/share/zoneinfo/tzdata.zi";
+	char tz[100];
+
+	//
+    FILE *cmd=popen(command, "r");
+
+	//
+	while(fgets(tz, 500, cmd))
+	{
+		//
+		printf("%s", tz);
+	}
+
+	//
+	pclose(cmd);
+}
+
+// Function 'getListOfAllKnownTZ' to get all available timezones
+/*char** getListOfAllKnownTZ()
+{
+	// 
+	char* command = "awk '/^Z/ { print $2 }; /^L/ { print $3 }' /usr/share/zoneinfo/tzdata.zi";
+	char* arrayOfTZ[500];
+	char* currentTZ;
+	int i = 0;
+
+	//
+	char tz[100];
+
+	//
+    FILE *cmd=popen(command, "r");
+
+	//
+	while(fgets(tz, 500, cmd))
+	{
+		printf("%s", tz);
+
+		//
+		arrayOfTZ[i] = tz;
+
+		printf("%s\n", arrayOfTZ[i]);
+
+		//
+		i++;
+	}
+
+	//
+	pclose(cmd);
+
+	//
+	return arrayOfTZ;
+}*/
+
+// Funtion 'getWorldClock_2_0ForWishedTZ' to get date and time for the wished timezone
+struct worldClock_2_0 getWorldClock_2_0ForWishedTZ(char* wishedTZ)
+{
+	// Definition of all necessary variables
+    char* worldClock_2_0ChoosenTZ = malloc(30);
+	char fullSize = strlen("TZ=") + strlen(" date") + strlen(" +%Y_%m_%d_%U_%u_%H_%M_%S") + strlen(wishedTZ);
+	char c;
+	int i = 0;
+	int j = 0;
+    FILE *cmd;
+	char* extrArray;
+	struct worldClock_2_0 resultWorldClock_2_0;
+	int valOfTrans = 0;
+
+	// Definition of the bash command to have date and time for the choosen timezone
+	char* completeCommand = (char*) malloc(fullSize);
+	strcat(completeCommand, "TZ=");
+	strcat(completeCommand, wishedTZ);
+	strcat(completeCommand, " date");
+	strcat(completeCommand, " +%Y_%m_%d_%U_%u_%H_%M_%S");
+
+	// Execution of the command "TZ={wishedTZ} date +'%Y_%m_%d_%U_%u_%H_%M_%S'" to have the date and time for the wished timezone via the 'cmd' file
+    cmd=popen(completeCommand, "r");
+
+	//
+    while((c = fgetc(cmd)) != EOF)
+	{
+		//
+    	worldClock_2_0ChoosenTZ[i] = c;
+
+		// Incrementation of the 'i' incrementor's value
+		i++;
+	}
+	
+	// Close the file 'cmd'
+    pclose(cmd);
+
+	// Conversion of the first extract element of the WorldClock_2_0 to an integer
+	extrArray = strtok(worldClock_2_0ChoosenTZ, "_");
+
+	// Loop to extract all elements of the WorldClock_2_0 to an integer and put them in the 'resultWorldClock_2_0' struct WorldClock_2_0
+	while(extrArray != NULL) {
+
+		//valOfTrans = (int)extrArray;
+		sscanf(extrArray, "%d", &valOfTrans);
+
+		//
+		switch(j)
+		{
+			case 0 : resultWorldClock_2_0.year = valOfTrans; break;
+			case 1 : resultWorldClock_2_0.month = valOfTrans; break;
+			case 2 : resultWorldClock_2_0.dmonth = valOfTrans; break;
+			case 3 : resultWorldClock_2_0.nweek = valOfTrans; break;
+			case 4 : resultWorldClock_2_0.dweek = valOfTrans; break;
+			case 5 : resultWorldClock_2_0.hour = valOfTrans; break;
+			case 6 : resultWorldClock_2_0.minute = valOfTrans; break;
+			case 7 : resultWorldClock_2_0.second = valOfTrans; break;
+		}
+
+		// 
+    	extrArray = strtok(NULL, "_");
+
+		// Incrementation of the 'j' incrementor's value
+		j++;
+   	}
+
+	// Return the date and time for the choosen timezone
+    return resultWorldClock_2_0;
+}
+
+// Funtion 'sprintfWorldClock_2_0' to return date and time from a struct WorldClock_2_0 object as a string
+char* sprintfWorldClock_2_0(struct worldClock_2_0 worldClock_2_0ToDisplay)
+{
+	// Definition of the 'resultString' string which will contain the result
+	char* resultString = malloc(100 * sizeof(char));
+
+	//
+	sprintf(resultString, "date : %d/%d/%d | time : %d:%d:%d | number of the day in the week : %d | number of the week in the year : %d |", worldClock_2_0ToDisplay.dmonth, worldClock_2_0ToDisplay.month, worldClock_2_0ToDisplay.year, worldClock_2_0ToDisplay.hour, worldClock_2_0ToDisplay.minute, worldClock_2_0ToDisplay.second, worldClock_2_0ToDisplay.dweek, worldClock_2_0ToDisplay.nweek);
+
+	// Return the date and time as a string
+	return resultString;
+}
