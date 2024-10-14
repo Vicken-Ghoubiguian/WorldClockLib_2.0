04509c461738fa74f66b6bc38bfc7d4fd9a89add
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-10-14 18:52:13 +0200

    HHGG

--- WorldClock_2.0/WorldClock_2.0.c
+++ WorldClock_2.0/WorldClock_2.0.c
@@ -145,7 +145,10 @@ char* sprintfWorldClock_2_0(struct worldClock_2_0 worldClock_2_0ToDisplay)
 	char* resultString = malloc(100 * sizeof(char));
 
 	//
-	sprintf(resultString, "date : %d/%d/%d | time : %d:%d:%d | number of the day in the week : %d | number of the week in the year : %d |", worldClock_2_0ToDisplay.dmonth, worldClock_2_0ToDisplay.month, worldClock_2_0ToDisplay.year, worldClock_2_0ToDisplay.hour, worldClock_2_0ToDisplay.minute, worldClock_2_0ToDisplay.second, worldClock_2_0ToDisplay.dweek, worldClock_2_0ToDisplay.nweek);
+	sprintf(s_year, "%d", worldClock_2_0ToDisplay.year);
+
+	//
+	sprintf(resultString, "date : %d/%d/%s | time : %d:%d:%d | number of the day in the week : %d | number of the week in the year : %d |", worldClock_2_0ToDisplay.dmonth, worldClock_2_0ToDisplay.month, s_year, worldClock_2_0ToDisplay.hour, worldClock_2_0ToDisplay.minute, worldClock_2_0ToDisplay.second, worldClock_2_0ToDisplay.dweek, worldClock_2_0ToDisplay.nweek);
 
 	// Return the date and time as a string
 	return resultString;
