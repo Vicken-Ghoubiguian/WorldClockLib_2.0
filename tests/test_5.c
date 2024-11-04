85cf713802d1b32f6af0442c1bad63b9a8a3a6de
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-11-04 18:32:44 +0100

    NbbVb

--- tests/test_5.c
+++ tests/test_5.c
@@ -17,7 +17,21 @@ int main()
     currentOS = getCurrentOS();
 
     //
-    
+    switch(currentOS)
+    {
+        case unix_1 : printf("Unix 1\n\n"); break;
+        case unix_2 : printf("Unix 2\n\n"); break;
+        case unix_3 : printf("Unix 3\n\n"); break;
+        case macOS_apple : printf("MacOS Apple\n\n"); break;
+        case macOS_mach : printf("MacOS Mach\n\n"); break;
+        case windows_32 : printf("Windows 32\n\n"); break;
+        case windows_64 : printf("Windows 64\n\n"); break;
+        case cygwin : printf("Cygwin\n\n"); break;
+        case linux : printf("Linux\n\n"); break;
+        case android : printf("Android\n\n"); break;
+        case freeBSD : printf("FreeBSD\n\n"); break;
+        default : printf("Unknown\n\n"); break;
+    }
 
 	//
 	return 0;
