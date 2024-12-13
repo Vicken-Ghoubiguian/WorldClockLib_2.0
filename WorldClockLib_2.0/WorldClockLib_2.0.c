ff657264dad5cbe8f76f7fdc70324c97b4cbb457
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2024-12-13 19:51:03 +0100

    Fyone

--- WorldClockLib_2.0/WorldClockLib_2.0.c
+++ WorldClockLib_2.0/WorldClockLib_2.0.c
@@ -285,7 +285,7 @@ char* sprintfWorldClock_2_0(struct worldClock_2_0 worldClock_2_0ToDisplay)
 	s_timezone = worldClock_2_0ToDisplay.timezone;
 
 	//
-	sprintf(resultString, "\033[0;32m{timezone : %s | date : %s/%s/%s | time : %s:%s:%s | number of the day in the week : %s | number of the week in the year : %s | number of the day in the year : %s}\033[0m", s_timezone, s_dmonth, s_month, s_year, s_hour, s_minute, s_second, s_dweek, s_nweek, s_dyear);
+	sprintf(resultString, "%s{timezone : %s | date : %s/%s/%s | time : %s:%s:%s | number of the day in the week : %s | number of the week in the year : %s | number of the day in the year : %s}%s", GREEN, s_timezone, s_dmonth, s_month, s_year, s_hour, s_minute, s_second, s_dweek, s_nweek, s_dyear, RESET);
 
 	// Return the date and time as a string
 	return resultString;
@@ -313,7 +313,7 @@ char* sprintfLike_time_t(struct like_time_t time_tToDisplay)
 	char* resultString = malloc(150 * sizeof(char));
 
 	//
-	sprintf(resultString, "\033[0;32m{timezone : %s | datetime : %ld | number of the week in the year : %s}\033[0m", s_timezone, time_tToDisplay.dt_as_time_t, s_nweek);
+	sprintf(resultString, "%s{timezone : %s | datetime : %ld | number of the week in the year : %s}%s", GREEN, s_timezone, time_tToDisplay.dt_as_time_t, s_nweek, RESET);
 
 	// Return the 'resultString' string result
 	return resultString;
