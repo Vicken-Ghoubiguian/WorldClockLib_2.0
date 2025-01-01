66b1334de08a99725a48252dded0324de7cb4581
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2025-01-01 11:55:33 +0100

    Little precision for future modification

--- WorldClockLib_2.0/WorldClockLib_2.0.h
+++ WorldClockLib_2.0/WorldClockLib_2.0.h
@@ -11,11 +11,15 @@ char* getListOfAllKnownTZ();
 // Function 'isWishedTZExist' to check if the 'wishedTZ' timezone exists or not
 bool isWishedTZExist(char* wishedTZ);
 
+// ============================== TO REPLACE BY SHELL =======================================
+
 // Definition of the 'OS' enum to determine the current operating system
 enum OS {undefined, unix_1, unix_2, unix_3, macOS_apple, macOS_mach, windows_32, windows_64, cygwin, linux, android, freeBSD};
 
 // Definition of the 'getCurrentOS' to return the current operating system as 'enum OS'
 enum OS getCurrentOS();
+
+// ==========================================================================================
 
 // Definition of the 'worldClock_2_0' struct to define and manipulate results
 struct worldClock_2_0
